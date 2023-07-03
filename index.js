ec09cdd87c1351a70a880f6f0abb698bbf7acce9
long.dang <84171134+longdangdjv@users.noreply.github.com>
2023-07-03 07:54:54 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-const express = require('express');
+import express from 'express';
+import os from 'os';
+
 const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
+  const clientIp = req.header('x-forwarded-for');
+  const elbIp = req.socket.remoteAddress;
+  const containerIp = req.socket.localAddress;
+  const containerName = os.hostname();
+
   console.log("Hello Root!!");
-  res.json('Hello World!');
+
+  res.json({
+    serviceName: 'LongDD_JP Demo ECS',
+    clientIp: clientIp,
+    elbIp: elbIp,
+    containerIp: containerIp,
+    containerName: containerName
+  });
+})
+
+app.get('/user', (req, res) => {
+  const clientIp = req.header('x-forwarded-for');
+  const elbIp = req.socket.remoteAddress;
+  const containerIp = req.socket.localAddress;
+  const containerName = os.hostname();
+
+  console.log("Hello User Service!!");
+
+  res.json({
+    serviceName: 'LongDD_JP Demo ECS User Service',
+    clientIp: clientIp,
+    elbIp: elbIp,
+    containerIp: containerIp,
+    containerName: containerName
+  });
+})
+
+app.get('/email', (req, res) => {
+  const clientIp = req.header('x-forwarded-for');
+  const elbIp = req.socket.remoteAddress;
+  const containerIp = req.socket.localAddress;
+  const containerName = os.hostname();
+
+  console.log("Hello Email Service!!");
+
+  res.json({
+    serviceName: 'LongDD_JP Demo ECS Email Service',
+    clientIp: clientIp,
+    elbIp: elbIp,
+    containerIp: containerIp,
+    containerName: containerName
+  });
 })
 
 app.listen(port, () => {
